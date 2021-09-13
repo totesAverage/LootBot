61de11b38c1bc6ba5b93113a594e4c70a819610b
Imeel Saleem <87979781+totesAverage@users.noreply.github.com>
2021-09-13 21:18:02 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,36 +1,23 @@
 import discord
 from discord.ext import commands, tasks
 import os
-import random
 
 #random loot with dice 
-client = commands.Bot(command_prefix='/')
+thing = discord.Activity(type=discord.ActivityType.listening, name="DnD!")
 
-@client.command(name='hello', help='This command returns a random welcome message')
-async def hello(ctx):
-    responses = 'Hello World!'
-    await ctx.send(responses)
+client = commands.Bot(command_prefix='#', activity=thing, status=discord.Status.online)
 
-# Rolls dice in NdN format
-@client.command(aliases=['r'])
-async def roll(ctx, dice: str):
-  # gets the number of rolls (first N) and the dice type (second N)
-  # and stores them in two separate values. Map function applies the int function without needing a loop
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    # To prevent any format other than NdN
-    except Exception:
-        await ctx.send('Incorrect input. Please type in "NdN" format')
-        return
+@client.command()
+async def load(ctx, extension):
+  client.load_extension(f'cogs.{extension}')
 
-    s = []
-    # For the number of rolls, the for loop will add a number to the empty list as a string
-    for r in range(rolls):
-      test = random.randint(1, limit)
-      s.append(str(test))
-    # joins all the strings in the list with a , between them
-    result = ', '.join(s)
-    await ctx.send("You rolled: " + str(result))
+@client.command()
+async def unload(ctx, extension):
+  client.unload_extension(f'cogs.{extension}')
 
+for filename in os.listdir('./cogs'):
+  if filename.endswith('.py'):
+    client.load_extension(f'cogs.{filename[:-3]}')
+    
 
 client.run(os.getenv('lootbot_token'))
