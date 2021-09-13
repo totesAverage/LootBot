eb7276146826db961b4f85aec1f67c43e3f52507
Imeel Saleem <87979781+totesAverage@users.noreply.github.com>
2021-09-13 22:10:31 +0500

    Update roller.py

--- cogs/roller.py
+++ cogs/roller.py
@@ -5,25 +5,31 @@ import random
 
 class roller(commands.Cog):
 
+  # Initialize the cog
   def __init__(self, client):
     self.client = client
 
+  # Rolls dice in NdN format  
   @commands.command(aliases=['r'], help='LootBot will roll dice for you!')
   async def roll(self, ctx, dice: str):
 
+    # This will split the input into two integers, designating the first N to number of rolls and the second N to dice limit.
     try:
       rolls, limit = map(int, dice.split('d'))
 
     except Exception:
       await ctx.send('Incorrect input. Please type in "NdN" format')
       return
-
+    
+    # Create an empty array
     s = []
 
+    # For the number of times of rolls, use randint to pick a number from 1 to the limit. It will only pick whole numbers. Put them in the empty list as strings.
     for r in range(rolls):
       test = random.randint(1, limit)
       s.append(str(test))
 
+    # Join the results in the list together and store it to send
     result = ', '.join(s)
     await ctx.send("You rolled: " + str(result))
 
